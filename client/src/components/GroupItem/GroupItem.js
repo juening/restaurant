57faa37e20ecf0deda84bbc5c6ef05232ac8bf1c
Jue Ning <juening@gmail.com>
2020-09-08 22:05:08 -0400

    title dispaly bug fixed, padding added to the app

--- client/src/components/GroupItem/GroupItem.js
+++ client/src/components/GroupItem/GroupItem.js
@@ -7,7 +7,7 @@ const GroupItem = ({ id, name, price, imageUrl }) => {
     <div className="group-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className="group-footer">
-        {/* <span className="name">{name}</span> */}
+        <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
     </div>
