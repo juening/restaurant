29e3d054d886fb777f9a4af92aebea6071595058
Jue Ning <juening@gmail.com>
2020-09-20 19:51:19 -0400

    items in bag reduced to itemCount in bag

--- client/src/components/ShoppingBag/ShoppingBag.js
+++ client/src/components/ShoppingBag/ShoppingBag.js
@@ -7,15 +7,19 @@ import { ReactComponent as BagIcon } from '../../assets/ShoppingBag.svg';
 
 import './ShoppingBag.scss';
 
-const ShoppingBag = ({ toggleBagHidden }) => (
+const ShoppingBag = ({ toggleBagHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleBagHidden}>
     <BagIcon className="bag-icon" />
-    <div className="item-count">0</div>
+    <div className="item-count">{itemCount}</div>
   </div>
 );
+
+const mapStateToProps = (state) => ({
+  itemCount: state.bag.bagItems.reduce((acc, item) => acc + item.quantity, 0),
+});
 
 const mapDispatchToProps = (dispatch) => ({
   toggleBagHidden: () => dispatch(toggleBagHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(ShoppingBag);
+export default connect(mapStateToProps, mapDispatchToProps)(ShoppingBag);
